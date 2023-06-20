97a01aa3c604860c4d4cd0e6647ac98d0f9b251b
KishwarEdgeforce <kishwari@edgeforce.in>
2023-06-20 15:35:31 +0530

    Updates

---
+++ precache-manifest.052ee6b0bf919addbf6086d5e70f7c71.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0d6fd868756ab1e24c78a472c9ac6d",
+    "url": "/accident/index.html"
+  },
+  {
+    "revision": "0c006747f26ea3d35239",
+    "url": "/accident/static/css/14.e997acfc.chunk.css"
+  },
+  {
+    "revision": "106234eed3e9434aacf3",
+    "url": "/accident/static/css/3.8beb1efa.chunk.css"
+  },
+  {
+    "revision": "5af658df627fde1e1647",
+    "url": "/accident/static/css/4.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "68e5412c85840371206e",
+    "url": "/accident/static/css/main.21fc41da.chunk.css"
+  },
+  {
+    "revision": "1a35e1d51387e439dee5",
+    "url": "/accident/static/js/0.7d18a16c.chunk.js"
+  },
+  {
+    "revision": "d93c20da77bbf643e044",
+    "url": "/accident/static/js/10.f41ab8b6.chunk.js"
+  },
+  {
+    "revision": "5a92cf0e47087690b783",
+    "url": "/accident/static/js/11.bdacf2aa.chunk.js"
+  },
+  {
+    "revision": "2a6393687494af0038ce",
+    "url": "/accident/static/js/12.1f9fb90a.chunk.js"
+  },
+  {
+    "revision": "8beee088ca2603624afd",
+    "url": "/accident/static/js/13.145e11c0.chunk.js"
+  },
+  {
+    "revision": "0c006747f26ea3d35239",
+    "url": "/accident/static/js/14.01630140.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/accident/static/js/14.01630140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a23b6efd73d84b3e0dcf",
+    "url": "/accident/static/js/15.5bca88db.chunk.js"
+  },
+  {
+    "revision": "d1ffb5e41957dcde7988",
+    "url": "/accident/static/js/16.6f046347.chunk.js"
+  },
+  {
+    "revision": "5b8dc1546f3e758d81c5",
+    "url": "/accident/static/js/17.63348d81.chunk.js"
+  },
+  {
+    "revision": "7b8d3e3b049442274bf1",
+    "url": "/accident/static/js/18.383409f3.chunk.js"
+  },
+  {
+    "revision": "729f8e007477211c107c",
+    "url": "/accident/static/js/19.c934adb4.chunk.js"
+  },
+  {
+    "revision": "5865d6b7e4ca70dd9ec1",
+    "url": "/accident/static/js/20.b8e854c6.chunk.js"
+  },
+  {
+    "revision": "6abd7d5c6cbf4594a9e3",
+    "url": "/accident/static/js/21.ea34e241.chunk.js"
+  },
+  {
+    "revision": "106234eed3e9434aacf3",
+    "url": "/accident/static/js/3.dd8ab935.chunk.js"
+  },
+  {
+    "revision": "eac667376cad77e0c575b555a7f71f3e",
+    "url": "/accident/static/js/3.dd8ab935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5af658df627fde1e1647",
+    "url": "/accident/static/js/4.0f57f641.chunk.js"
+  },
+  {
+    "revision": "9d4b6c96660161631dd1f9f21c2daac8",
+    "url": "/accident/static/js/4.0f57f641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea91d718e681cc6a1d3",
+    "url": "/accident/static/js/5.bc61bf94.chunk.js"
+  },
+  {
+    "revision": "ac01945a4bfeeccb9d70ac6406ad175a",
+    "url": "/accident/static/js/5.bc61bf94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "907058721cf5ceb11487",
+    "url": "/accident/static/js/6.ddc31cd1.chunk.js"
+  },
+  {
+    "revision": "0153eb62d520542d205d",
+    "url": "/accident/static/js/7.712a13a4.chunk.js"
+  },
+  {
+    "revision": "1287c5cc74376c1faed4",
+    "url": "/accident/static/js/8.ddc557ec.chunk.js"
+  },
+  {
+    "revision": "3f59fde92ceff02b3b94",
+    "url": "/accident/static/js/9.cb8f824e.chunk.js"
+  },
+  {
+    "revision": "68e5412c85840371206e",
+    "url": "/accident/static/js/main.5087a167.chunk.js"
+  },
+  {
+    "revision": "127052f9b77422e4faf6",
+    "url": "/accident/static/js/runtime-main.b4ffedf6.js"
+  },
+  {
+    "revision": "a50df54739609acd4f7ecd8a4f36c4e2",
+    "url": "/accident/static/media/asc1.a50df547.png"
+  },
+  {
+    "revision": "cbf9c83a4aea661d4c61e3272e5fe789",
+    "url": "/accident/static/media/avatar-1.cbf9c83a.jpg"
+  },
+  {
+    "revision": "034d02d73fd503ee64391c76941432bc",
+    "url": "/accident/static/media/avatar-2.034d02d7.jpg"
+  },
+  {
+    "revision": "7ce49759d33047eada521f43b0aa3294",
+    "url": "/accident/static/media/avatar-3.7ce49759.jpg"
+  },
+  {
+    "revision": "04dc3c8128985b17e4cf6b2060eb40ba",
+    "url": "/accident/static/media/client1.04dc3c81.png"
+  },
+  {
+    "revision": "ae216552dd7035ef2e18c4799c165501",
+    "url": "/accident/static/media/logo.ae216552.jpg"
+  }
+]);
